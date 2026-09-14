4de478bd66dcb97d66a303feb135131aa7d0887e
Nathan Katov <nkato8001@gmail.com>
2026-09-14 16:10:41 -0400

    Implemented heuristic methods

--- aux.py
+++ aux.py
@@ -1,1 +1,19 @@
-def dist()
+import math
+
+def distance(p, q):
+    plat, plon = p
+    qlat, qlon = q
+
+    dlat, dlon = qlat - plat, qlon - plon
+
+    intermediate = (math.sin(dlat / 2) ** 2) + math.cos(plat) * math.cos(qlat) * (math.sin(dlon / 2) ** 2)
+
+    angle = 2 * math.atan2(math.sqrt(intermediate), math.sqrt(1 - intermediate))
+    return angle # distance is radius * angle of the sector
+
+def total_distance(cities):
+    res = 0
+    for i in range(0, len(cities)):
+        res += distance(cities[i - 1], cities[i])
+
+    return res
--- heuristics.py
+++ heuristics.py
@@ -1,5 +1,58 @@
+from aux import distance
+import math
 
+def create_distance_matrix(cities):
+    n = len(cities)
+    dist = [[0 for j in range(n)] for i in range(n)]
 
+    for i in range(n):
+        for j in range(i + 1, n):
+            dist[i][j] = dist[j][i] = distance(cities[i], cities[j])
 
-def two_opt(order, lat, lon):
+    return dist
 
+def two_opt(order, cities):
+
+    D = create_distance_matrix(cities)
+
+    n = len(order)
+    while True:
+        improved = False
+        for i in range(n - 1):
+            for j in range(i + 2, n):
+                a, b, c, d = order[i], i + 1, j, (j + 1) % n
+                delta = - distance(cities[a], cities[b]) - distance(cities[c], cities[d]) + distance(cities[a], cities[c]) + distance(cities[b], cities[d])
+
+                if (delta < 0):
+                    order = order[:i + 1] + order[i + 1:j + 1][::-1] + order[j + 1:]
+                    improved = True
+
+        if not improved:
+            break
+
+    return order
+
+def nearest_neighbour(cities):
+    n = len(cities)
+
+    dist = create_distance_matrix(cities)
+    
+    cur = 0
+    order = [cur]
+
+    seen = set()
+    seen.add(cur)
+
+    for t in range(n - 1):
+        nxt, best = -1, 2 * math.pi
+        for i in range(n):
+            if i in seen:
+                continue
+
+            if (best < dist[cur][i]):
+                nxt = i
+                best = dist[cur][nxt]
+
+        order.append(nxt)
+        seen.add(nxt)
+        cur = nxt
